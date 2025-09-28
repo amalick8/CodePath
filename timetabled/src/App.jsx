7eb53b1eaae8ea69e2f58a8adc2f1582ba2071fa
Ammar Malick <ammarmalick2006@gmail.com>
2025-09-28 01:40:18 -0500

    initial commit

--- timetabled/src/App.jsx
+++ timetabled/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
-
+import Calendar from './components/Calendar';
 const App = () => {
 
   return (
     <div className="App">
-
-      
+      <h1>School Itenerary 📚</h1>
+      <h2>Here is an itenerary of my 7 days as a college student 😢</h2>
+      <Calendar />
     </div>
   )
 }
---
+++ timetabled/src/components/Calendar.jsx
@@ -0,0 +1,8 @@
+const Calendar = () => {
+
+    return (
+        <div className='Calendar'>Testing the calendar component</div>
+    )
+}
+
+export default Calendar;
