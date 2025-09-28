de502eda6337fcaacb19a929f6e47d9b299e96b8
Ammar Malick <ammarmalick2006@gmail.com>
2025-09-28 01:58:43 -0500

    added headers to the calendar

--- timetabled/src/components/Calendar.jsx
+++ timetabled/src/components/Calendar.jsx
@@ -1,8 +1,25 @@
 const Calendar = () => {
 
     return (
-        <div className='Calendar'>Testing the calendar component</div>
-    )
+      <div className='Calendar'>
+        <table>
+          <thead>
+            <tr>
+              <th>Sunday</th>
+              <th>Monday</th>
+              <th>Tuesday</th>
+              <th>Wednesday</th>
+              <th>Thursday</th>
+              <th>Friday</th>
+              <th>Saturday</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* Add your table rows here */}
+          </tbody>
+        </table>
+      </div>
+  )
 }
 
 export default Calendar;
